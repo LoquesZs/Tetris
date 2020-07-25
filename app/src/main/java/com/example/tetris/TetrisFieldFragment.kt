a56dd867e38a558f98393d82405525e4fa532f5c
Anton Polubok <ant.polubok@yandex.rugit config --global user.email ant.polubok@yandex.rugit config --global user.email ant.polubok@yandex.ru>
2020-07-25 11:52:50 +0300

    Add TetrisFieldFragment.kt and change its layout file, add navigation.xml

--- app/src/main/java/com/example/tetris/TetrisFieldFragment.kt
+++ app/src/main/java/com/example/tetris/TetrisFieldFragment.kt
@@ -6,18 +6,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
-class NavHostFragment : Fragment() {
+class TetrisFieldFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-            }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
-
-        return inflater.inflate(R.layout.fragment_nav_host, container, false)
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_tetris_field, container, false)
     }
 }
