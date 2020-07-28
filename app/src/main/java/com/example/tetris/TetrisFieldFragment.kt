73c6a89fdfabb4e6302495df9b5cfa3c6ca9619f
Anton Polubok <ant.polubok@yandex.rugit config --global user.email ant.polubok@yandex.rugit config --global user.email ant.polubok@yandex.ru>
2020-07-28 17:27:02 +0300

    Move tetris logic from activities into fragments Add navigation support Add navigation graph, made it work Delete all unnecessary files Add new cup icon Add animations, replace animations

--- app/src/main/java/com/example/tetris/TetrisFieldFragment.kt
+++ app/src/main/java/com/example/tetris/TetrisFieldFragment.kt
@@ -1,22 +1,389 @@
 package com.example.tetris
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.util.Log
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import androidx.navigation.findNavController
+import kotlinx.android.synthetic.main.fragment_tetris_field.*
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlin.random.Random
+import kotlin.random.nextInt
 
 class TetrisFieldFragment : Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
+    private val bestScoreStorage = "Best Score Storage"  // key to best score storage in shared preferences
+    private lateinit var sharedPreferences: SharedPreferences
+    private var bestScore = "0" //best score loaded from shared preferences if they're not empty
+    private var score = 0 //game score variable
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
+    private var globalX = 4 // X position of figure on tetris field
+    private var globalY = 0 // Y position of figure on tetris field
+
+    private val startSpeed = 400L // start delay time in ms
+    private var speed = startSpeed // game speed variable
+    private val speedMagnifier = 5 // changes speed by its value
+
+    private var nextDrop = false // indicate if next figure have to dropdown
+    private val pausableDispatcher = PausableDispatcher(Handler(Looper.getMainLooper())) //dispatcher for figuresFallCoroutine that allows pauses in executing coroutine
+    private lateinit var figuresFallCoroutine: Job  // coroutine that drops down figure
+
+    private var figureIndex = 0 // index of current figure on field
+    private var figures = arrayListOf (
+        arrayListOf(0 to 0, 0 to -1, 0 to -2, 0 to -3),     // |      фигуры
+        arrayListOf(0 to 0, 1 to 0, 2 to 0, 3 to 0),        // ....
+        arrayListOf(0 to 0, 0 to -1, 0 to -2, 0 to -3),     // |
+        arrayListOf(0 to 0, 1 to 0, 2 to 0, 3 to 0),        // ....
+
+        arrayListOf(0 to 0, 1 to 0, 2 to 0, 1 to -1),       // .:.
+        arrayListOf(1 to 0, 0 to -1, 1 to -1, 1 to -2),     // -|
+        arrayListOf(1 to 0, 0 to -1, 1 to -1, 2 to -1),     // *:*
+        arrayListOf(0 to 0, 0 to -1, 1 to -1, 0 to -2),     // |-
+
+        arrayListOf(0 to 0, 1 to 0, 1 to -1, 2 to -1),      // .:'
+        arrayListOf(1 to 0, 0 to -1, 1 to -1, 0 to -2),     // ',
+        arrayListOf(0 to 0, 1 to 0, 1 to -1, 2 to -1),      // .:'
+        arrayListOf(1 to 0, 0 to -1, 1 to -1, 0 to -2),     // ',
+
+        arrayListOf(0 to 0, 1 to 0, 0 to -1, 1 to -1),      // ::
+        arrayListOf(0 to 0, 1 to 0, 0 to -1, 1 to -1),      // ::
+        arrayListOf(0 to 0, 1 to 0, 0 to -1, 1 to -1),      // ::
+        arrayListOf(0 to 0, 1 to 0, 0 to -1, 1 to -1),      // ::
+
+        arrayListOf(0 to 0, 0 to -1, 1 to -1, 1 to -2),     // ,'
+        arrayListOf(1 to 0, 2 to 0, 0 to -1, 1 to -1),      // *:.
+        arrayListOf(0 to 0, 0 to -1, 1 to -1, 1 to -2),     // ,'
+        arrayListOf(1 to 0, 2 to 0, 0 to -1, 1 to -1),      // *:.
+
+        arrayListOf(0 to 0, 1 to 0, 2 to 0, 0 to -1),       // :..
+        arrayListOf(0 to 0, 1 to 0, 1 to -1, 1 to -2),      // .|
+        arrayListOf(2 to 0, 0 to -1, 1 to -1, 2 to -1),     // **:
+        arrayListOf(0 to 0, 0 to -1, 0 to -2, 1 to -2),     // |*
+
+        arrayListOf(0 to 0, 1 to 0, 2 to 0, 2 to -1),       // ..:
+        arrayListOf(1 to 0, 1 to -1, 0 to -2, 1 to -2),     // *|
+        arrayListOf(0 to 0, 0 to -1, 1 to -1, 2 to -1),     // :**
+        arrayListOf(0 to 0, 1 to 0, 0 to -1, 0 to -2)       // |_
+    ) // array of all available figures
+    private var figure = getRandomFigure() // x to y
+
+    private var isSpeedUpButtonDown = false
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_tetris_field, container, false)
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        figuresFallCoroutine = GlobalScope.launch(pausableDispatcher) { figureDrop() }
+        super.onViewCreated(view, savedInstanceState)
+        score_display.text = score.toString()
+
+        loadBestScore()
+
+        best_score_display.text = bestScore
+
+        left_button.setOnClickListener {
+            if (globalY + figure.last().second !in 0 until yCellCount ||
+                globalX == 0 ||
+                isLeftCellFilled()) return@setOnClickListener else {
+                clearFieldFromFigure()
+                globalX--
+                nextDropSetter()
+                drawFigure()
+            }
+        }
+
+        rotate_button.setOnClickListener {
+            val currentFigureState = getCurrentFigureState()
+            val rotatedFigureIndex = if (currentFigureState == 1) figureIndex + 3 else figureIndex - 1
+            val nextFigure = figures[rotatedFigureIndex]
+            try {
+                for (point in nextFigure.indices) {
+                    val yIndex = globalY + nextFigure[point].second
+                    val xIndex = globalX + nextFigure[point].first
+                    if (tetrisField.field[xIndex][yIndex] == 1
+                        && tetrisField.field[xIndex][yIndex + 1] == 1
+                        && !figure.isIndicesInFigure(nextFigure[point].first, nextFigure[point].second)) return@setOnClickListener
+                }
+                clearFieldFromFigure()
+                figure = figures[rotatedFigureIndex]
+                figureIndex = rotatedFigureIndex
+                nextDropSetter()
+                drawFigure()
+            } catch(e: ArrayIndexOutOfBoundsException) {
+                return@setOnClickListener
+            }
+        }
+
+        right_button.setOnClickListener {
+            if (globalY + figure.last().second !in (0 until yCellCount) ||
+                globalX + figure.getRightXIndex() == xCellCount - 1 ||
+                isRightCellFilled()) return@setOnClickListener else {
+                clearFieldFromFigure()
+                globalX++
+                nextDropSetter()
+                drawFigure()
+            }
+        }
+
+        new_game_button.setOnClickListener {
+            figuresFallCoroutine.cancel()
+            saveBestScore()
+            figure = getRandomFigure()
+            tetrisField.clearArray()
+            tetrisField.invalidate()
+            score = 0
+            score_display.text = score.toString()
+            speed = startSpeed
+            globalX = 4
+            figuresFallCoroutine = GlobalScope.launch(pausableDispatcher) { figureDrop() }
+        }
+
+        speed_up_button.setOnTouchListener { v, event ->
+            v.performClick()
+            when (event?.action) {
+                MotionEvent.ACTION_DOWN -> {
+                    isSpeedUpButtonDown = true
+                    speed /= speedMagnifier
+                }
+
+                MotionEvent.ACTION_UP -> {
+                    speed *= speedMagnifier
+                    isSpeedUpButtonDown = false
+                }
+            }
+            v?.onTouchEvent(event) ?: true
+        }
+
+        pause_button.setOnClickListener {
+            it.findNavController().navigate(R.id.action_tetrisFieldFragment_to_pauseMenu)
+        }
+
+        figuresFallCoroutine
+    }
+
+    override fun onPause() {
+        super.onPause()
+        pausableDispatcher.pause()
+        Log.d("Pause", "Something wrong")
+        saveBestScore()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        pausableDispatcher.resume()
+        Log.d("Pause", "Something wrong")
+        loadBestScore()
+    }
+
+    /***********************************************
+
+                        tetris utils
+
+     ************************************************/
+
+    private suspend fun figureDrop() {
+        scoreUp(4)
+        for (y in 0 until yCellCount) {
+            nextDropCheck()
+            setButtonsState(true)
+            if (!nextDrop) {
+                if (y in 1 until yCellCount) clearFieldFromFigure()
+                globalY = y
+                drawFigure()
+                nextDropSetter()
+            }
+            tetrisField.invalidate()
+            delay(speed)
+            setButtonsState(false)
+            if (nextDrop && y == 0) {
+                figuresFallCoroutine.cancel()
+                return
+            }
+            nextDropCheck()
+            tetrisField.invalidate()
+        }
+    }
+
+    private fun drawFigure() {
+        for (point in figure.indices) {
+            val yIndex = globalY + figure[point].second
+            val xIndex = globalX + figure[point].first
+            if (yIndex in 0 until yCellCount) tetrisField.field[xIndex][yIndex] = 1
+        }
+
+        tetrisField.invalidate()
+    }
+
+    private fun nextDropSetter() {
+        var nextDropCheckSum = 0
+        for (point in figure.indices) {
+            val yIndex  = globalY + figure[point].second
+            val xIndex = globalX + figure[point].first
+            if (
+                yIndex in 0 until yCellCount-1 &&
+                !figure.isIndicesInFigure (figure[point].first, figure[point].second + 1) &&
+                (tetrisField.field[xIndex][yIndex + 1] == 1))  nextDropCheckSum++
+        }
+        nextDrop = when {
+            nextDropCheckSum == 0 && globalY in 0 until yCellCount-1 -> false
+            else -> true
+        }
+
+        tetrisField.invalidate()
+    }
+
+    private fun clearFieldFromFigure() {
+        for (point in figure.indices) {
+            val yIndex = globalY + figure[point].second
+            val xIndex = globalX + figure[point].first
+            if (yIndex in 0 until yCellCount) tetrisField.field[xIndex][yIndex] = 0
+        }
+        tetrisField.invalidate()
+    }
+
+    private fun ArrayList<Pair<Int, Int>>.isIndicesInFigure(x: Int, y: Int): Boolean {
+        var isIndicesInFigure = false
+        this.forEach { if (it.first == x && it.second == y) {
+            isIndicesInFigure = true
+            return@forEach
+        } }
+        return isIndicesInFigure
+    }
+
+    private fun ArrayList<Pair<Int, Int>>.getRightXIndex(): Int {
+        var rightXIndex = -1
+        this.forEach {
+            if (it.first > rightXIndex) rightXIndex = it.first
+        }
+        return rightXIndex
+    }
+
+    private suspend fun lineFillCheck() {
+        var sum = 0
+        for (y in 0 until yCellCount) {
+            for (x in 0 until xCellCount) {
+                sum += tetrisField.field[x][y]
+                if (x == (xCellCount-1) && sum == xCellCount) {
+                    delay (speed)
+                    tetrisField.invalidate()
+                    for (innerY in y downTo 0) {
+                        for (innerX in 0 until xCellCount) {
+                            if (innerY > 0) tetrisField.field[innerX][innerY] = tetrisField.field[innerX][innerY - 1]
+                        }
+                    }
+                    scoreUp(100)
+                    tetrisField.invalidate()
+                }
+            }
+            sum = 0
+        }
+    }
+
+    private fun getRandomFigure(): ArrayList<Pair<Int, Int>> {
+        figureIndex = Random.nextInt(0..figures.lastIndex)
+        return figures[figureIndex]
+    }
+
+    private fun isLeftCellFilled(): Boolean {
+        var isLeftCellFilled = false
+        for (point in figure.indices) {
+            val xIndex = globalX + figure[point].first
+            val yIndex = globalY + figure[point].second
+            if (yIndex in 1 until yCellCount) {
+                if(xIndex in 1 until xCellCount
+                    && !figure.isIndicesInFigure((figure[point].first - 1), (figure[point].second))
+                    && tetrisField.field[xIndex - 1][yIndex] == 1) isLeftCellFilled = true
+            }
+        }
+        return isLeftCellFilled
+    }
+
+    private fun isRightCellFilled(): Boolean {
+        var isRightCellFilled = false
+        for (point in figure.indices) {
+            val xIndex = globalX + figure[point].first
+            val yIndex = globalY + figure[point].second
+            if (globalY in 1 until yCellCount) {
+                if(xIndex in 0 until xCellCount-1
+                    && !figure.isIndicesInFigure((figure[point].first + 1), (figure[point].second))
+                    && tetrisField.field[xIndex + 1][yIndex] == 1) isRightCellFilled = true
+            }
+        }
+        return isRightCellFilled
+    }
+
+    private fun getCurrentFigureState(): Int {
+        return when ((figureIndex + 1) % 4) {
+            1 -> 1
+            2 -> 2
+            3 -> 3
+            else -> 4
+        }
+    }
+
+    private fun scoreUp(value: Int) {
+        speed -= when {
+            value == 100 && isSpeedUpButtonDown -> 1.toLong()
+            value == 100 && !isSpeedUpButtonDown -> speedMagnifier.toLong()
+            else -> 0
+        }
+        score += value
+        score_display.text = score.toString()
+    }
+
+    private suspend fun nextDropCheck() {
+        if (nextDrop) {
+            globalX = 4
+            lineFillCheck()
+            nextDrop = false
+            figure = getRandomFigure()
+            figureDrop()
+        }
+    }
+
+    private fun saveBestScore() {
+        sharedPreferences = activity!!.getPreferences(Context.MODE_PRIVATE)
+        val editor = sharedPreferences.edit()
+        with(editor) {
+            if (bestScore == "0" || bestScore.toInt() < score) bestScore = score.toString()
+            putString(bestScoreStorage, bestScore)
+            apply()
+            best_score_display.text = bestScore
+        }
+
+    }
+
+    private fun loadBestScore() {
+        sharedPreferences = activity!!.getPreferences(Context.MODE_PRIVATE)
+        bestScore = sharedPreferences.getString(bestScoreStorage, "0") ?: "0"
+    }
+
+    private fun setButtonsState(isClickable: Boolean) {
+        when (isClickable) {
+            true -> {
+                left_button.isClickable = true
+                right_button.isClickable = true
+                rotate_button.isClickable = true
+                speed_up_button.isClickable = true
+            }
+            false -> {
+                left_button.isClickable = false
+                right_button.isClickable = false
+                rotate_button.isClickable = false
+                speed_up_button.isClickable = false
+            }
+        }
+    }
 }
+
+//TODO: Привести код в более читабельный вид
+//TODO: GameOverFragment
+//TODO: Навести красоту
